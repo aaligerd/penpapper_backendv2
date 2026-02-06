a30ca62beae3ef6fdaf419b21d2f691a1572fd4d
Subhradip Majumder <86906345+aaligerd@users.noreply.github.com>
2026-02-06 19:51:29 +0530

    author fields changes

--- controller/authorController.js
+++ controller/authorController.js
@@ -17,24 +17,24 @@ const s3 = new S3Client({ region: REGION });
 * @param {import('express').Response} res - Express response
 */
 const addAuthor = async (req, res) => {
-    /** @type {{author_name:String,email:String,bio:String,social:Array<String>,unq_slug:String,org_code:String,user_code:String}} */
-    let { author_name, email, bio, social, unq_slug, org_code, user_code } = req.body;
-    if (!author_name || author_name.trim() === "" || !email || email.trim() === "" || !unq_slug || unq_slug.trim() === "" || !org_code || org_code.trim() === "") {
+    /** @type {{author_name:String,email:String,bio:String,active_status:Boolean,social_links:String,unq_slug:String,user_name:String,user_role:String,org_code:String,user_code:String}} */
+    let { author_name, email, bio, social_links, unq_slug,active_status,user_name,user_role, org_code, user_code } = req.body;
+    if (!author_name || author_name.trim() === "" || !email || email.trim() === "" || !unq_slug || unq_slug.trim() === "") {
         return res.status(500).send({ msg: "Please provide mandetory field information" });
     }
-    const insertQ = "insert into tbl_author(org_code,author_name,email,author_bio,social_links,author_slug) values ($1,$2,$3,$4,$5,$6)";
-    social = social && social.length >= 1 ? social.join(",") : null;
+    const insertQ = "insert into tbl_author(author_name,email,author_bio,social_links,author_slug,is_active,user_name,user_role) values ($1,$2,$3,$4,$5,$6,$7,$8)";
+    social = social_links && social_links.length >= 1 ? JSON.stringify(social_links): null;
     try {
-        const { rowCount } = await pgClient.query(insertQ, [org_code, author_name, email, bio || "", social, unq_slug]);
+        const { rowCount } = await pgClient.query(insertQ, [ author_name, email, bio || "", social, unq_slug,active_status,user_name,user_role]);
         if (rowCount === 0) {
             return res.status(500).send({ msg: "There is an error while inserting new author data." });
         } else {
-            createAdminLog('NEW AUTHOR CREATED', user_code);
+            // createAdminLog('NEW AUTHOR CREATED', user_code);
             return res.status(200).send({ msg: "New author created" });
         }
     } catch (error) {
         
-        return res.status(500).send({ msg: 'Error: ' + error.detail });
+        return res.status(500).send({ msg: 'Error: ' + error });
     }
 }
 
@@ -185,23 +185,23 @@ const getAuthorById = async (req, res) => {
 * @param {import('express').Response} res - Express response
 */
 const updateAuthorById = async (req, res) => {
-    /** @type {{author_id:String,author_name:String,email:String,author_bio:String,social:Array<String>,is_active:boolean,author_slug:String,}} */
-    const { author_id, author_name, email, author_bio = "", social, author_slug } = req.body;
+    /** @type {{author_id:String,author_name:String,email:String,author_bio:String,ssocial_linksocial:String,author_slug:String,user_name:String,user_role:String,active_status:Boolean}} */
+    let { author_id, author_name, email, author_bio = "", social_links, author_slug ,active_status,user_role,user_name} = req.body;
     if (!author_id || author_id.trim() === "" || !author_name || author_name.trim() === "" || !email || email.trim() === "" || !author_slug || author_slug.trim() === "") {
         return res.status(500).send({ msg: "Please provide mandetory fields." });
     }
-    let social_links = social && social.length >= 1 ? social.join(',') : null;
-    const updateAuthorQ = 'update tbl_author set author_name=$1,email=$2,author_bio=$3,social_links=$4,author_slug=$5,last_mod_at=NOW() where id=$6;';
+    social_links = social_links && social_links.length >= 1 ? JSON.stringify(social_links) : null;
+    const updateAuthorQ = 'update tbl_author set author_name=$1,email=$2,author_bio=$3,social_links=$4,is_active=$5,author_slug=$6,user_name=$7,user_role=$8,last_mod_at=NOW() where id=$9;';
 
     try {
-        const { rowCount } = await pgClient.query(updateAuthorQ, [author_name, email, author_bio, social_links, author_slug, author_id]);
+        const { rowCount } = await pgClient.query(updateAuthorQ, [author_name, email, author_bio, social_links,active_status, author_slug,user_name,user_role, author_id]);
         if (rowCount === 0) {
             return res.status(500).send({ msg: "Author data not updated." });
         } else {
             return res.status(200).send({ msg: "Author data updated." });
         }
     } catch (error) {
-        
+        console.log(error);
         return res.status(500).send({ msg: 'Error: ' + error.detail });
     }
 }
